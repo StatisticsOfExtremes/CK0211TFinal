064d4d3bc76e697ed680a76ada63eefd7b815b2e
unknown <alyssondasm@gmail.com>
2021-08-31 18:31:52 -0300

    mud nas global opt e no display

--- display.py
+++ display.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import getAnimais
 import getAdocao
 import getCandidato
@@ -39,45 +38,3 @@ def main():
 main()
 
 
-=======
-import getAnimais
-import getAdocao
-import getCandidato
-import os
-import globalOpt
-
-def getOptions():
-        opcao = input("""
-        1 - Cadastrar novos animais
-        2 - Consultar animais
-        3 - Adotar um animal
-        4 - Sair
-        """)
-        if opcao == "1":
-                globalOpt.renderBackground()
-                getAnimais.opcoesMenuAnimais()
-                getOptions()
-        elif opcao == "2":
-                globalOpt.renderBackground() 
-                getAnimais.listarAnimais()
-                getOptions()
-        elif opcao == "3":
-                globalOpt.renderBackground()
-                #getAdocao
-                getOptions()
-        elif opcao == '4':
-                return False
-            
-
-
-def main():
-        open = True
-        while open:
-              open = getOptions()
-              
-
-
-main()
-
-
->>>>>>> master
--- globalOpt.py
+++ globalOpt.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 welcomeStr = """
@@ -23,29 +22,3 @@ def renderBackground():
         limparTela()
         print(welcomeStr)
 
-=======
-import os
-
-welcomeStr = """
-   _____ _  _____    _____          _   _______ 
-  / ____(_)/ ____|  / ____|   /\   | | |__   __|
- | (___  _| (___   | |  __   /  \  | |    | |   
-  \___ \| |\___ \  | | |_ | / /\ \ | |    | |   
-  ____) | |____) | | |__| |/ ____ \| |____| |   
- |_____/|_|_____/   \_____/_/    \_\______|_|                                                                                               
-
-"""
-
-print(welcomeStr)
-
-def limparTela():
-    commando = 'clear'
-    if os.name in ('nt', 'dos'):  # Se tiver usando o windows o comando para limpar a tela é 'cls'
-        commando = 'cls'
-    os.system(commando)
-
-def renderBackground():
-        limparTela()
-        print(welcomeStr)
-
->>>>>>> master
